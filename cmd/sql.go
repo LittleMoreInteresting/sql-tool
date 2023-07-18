420cee601b1d4ac6c793c22fa80fb544d9c3c1d2
shr26207 <sunhaoran@offcn.com>
2023-07-18 11:11:18 +0800

    feat:readmd update

--- cmd/sql.go
+++ cmd/sql.go
@@ -36,6 +36,9 @@ var sqlToStructCmd = &cobra.Command{
 			log.Fatalf("dbModel.Connect error :%v", err)
 		}
 		template, _ := ioutil.ReadFile(tmpl)
+		if len(template) == 0 {
+			template = []byte(defaultTemp)
+		}
 		var dir string
 		if len(tableName) > 0 {
 			columns, err := dbModel.GetCloumns(dbName, tableName)
@@ -86,3 +89,5 @@ func init() {
 	sqlToStructCmd.Flags().StringVarP(&tableName, "table", "t", "", "请输入表名(不输入将全库导出)")
 	sqlToStructCmd.Flags().StringVarP(&tmpl, "tmpl", "m", "./template/model.tmpl", "模版文件")
 }
+
+var defaultTemp = "package {{ .Database }}\n\nimport (\n     \"context\"\n     \"gorm.io/gorm\"\n)\n\ntype {{ .TableName | ToCamelCase }} struct {\n\t{{range $index, $element := .Columns}}{{ if gt $index 0 }}\n\t{{ end }}{{ $typeLen := len .Type }}{{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}\t{{.Type}}\t{{.Tag}}{{ else }}{{.Name}}{{ end }}{{ $length := len .Comment}}{{ if gt $length 0 }}// {{ .Comment }}{{else}}// {{.Name}}{{ end }}{{end}}\n}\n\nfunc (model {{ .TableName | ToCamelCase}}) TableName() string {\n    return \"{{ .TableName }}\"\n}\n\ntype {{ .TableName | ToCamelCase }}Interface interface {\n    Create{{ .TableName | ToCamelCase }}(ctx context.Context,t *{{ .TableName | ToCamelCase }}) (*{{ .TableName | ToCamelCase }},error)\n    Get{{ .TableName | ToCamelCase }}(ctx context.Context,id int64) (*{{ .TableName | ToCamelCase }},error)\n    Update{{ .TableName | ToCamelCase }}(ctx context.Context,t *{{ .TableName | ToCamelCase }}) error\n    Delete{{ .TableName | ToCamelCase }}(ctx context.Context,id int64) error\n}\nvar _ {{ .TableName | ToCamelCase }}Interface = (*{{ .TableName | ToCamelCase }}Repo)(nil)\n\ntype {{ .TableName | ToCamelCase }}Repo struct {\n    db  *gorm.DB\n}\n\nfunc New{{ .TableName | ToCamelCase }}Repo(db *gorm.DB) *{{ .TableName | ToCamelCase }}Repo {\n    return &{{ .TableName | ToCamelCase }}Repo{\n        db:db,\n    }\n}\n\n// Get DB client\nfunc(m *{{ .TableName | ToCamelCase }}Repo)DB(ctx context.Context) *gorm.DB {\n    return m.db\n}\n\n//Create One record\nfunc (m *{{ .TableName | ToCamelCase }}Repo) Create{{ .TableName | ToCamelCase }}(ctx context.Context,t *{{ .TableName | ToCamelCase }}) (*{{ .TableName | ToCamelCase }},error){\n    err := m.DB(ctx).Create(t).Error\n    if err != nil {\n        return nil,err\n    }\n    return t,err\n}\n\n//Get One record\nfunc (m *{{ .TableName | ToCamelCase }}Repo) Get{{ .TableName | ToCamelCase }}(ctx context.Context,id int64) (*{{ .TableName | ToCamelCase }},error){\n    record := &{{ .TableName | ToCamelCase }}{}\n    err:= m.DB(ctx).First(record,id).Error\n    return record,err\n}\n\n//Update One record\nfunc (m *{{ .TableName | ToCamelCase }}Repo) Update{{ .TableName | ToCamelCase }}(ctx context.Context,t *{{ .TableName | ToCamelCase }}) error {\n    return m.DB(ctx).Updates(t).Error\n}\n\n//Delete One record\nfunc (m *{{ .TableName | ToCamelCase }}Repo) Delete{{ .TableName | ToCamelCase }}(ctx context.Context,id int64) error {\n    return m.DB(ctx).Delete(&{{ .TableName | ToCamelCase }}{},id).Error\n}"
